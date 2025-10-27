1f09936bb3e7a4d6343ad0937c44fcfcee20dffe
Parola-cod <gianlucananna@gmail.com>
2025-10-27 15:49:53 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const supabaseUrl = https://qrqpfektlgecupuhvotj.supabase.co;
+    const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFycXBmZWt0bGdlY3VwdWh2b3RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzNTMwOTIsImV4cCI6MjA3NjkyOTA5Mn0.Yv168Sr134HY7qi8PWuRByAQNHGkrvnLHmEGfF7dsjQ;
+    const sb = createClient(supabaseUrl, supabaseKey);
+	init();
